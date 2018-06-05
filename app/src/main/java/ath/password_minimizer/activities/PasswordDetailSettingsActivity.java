d89e15f4a5c81f593e2699382940dabd8f20f7b1
neni <neniverena@gmx.de>
2018-06-05 23:07:59 +0200

    Added basic navigation bar for pw detail websites / settings.

--- app/src/main/java/ath/password_minimizer/activities/PasswordDetailSettingsActivity.java
+++ app/src/main/java/ath/password_minimizer/activities/PasswordDetailSettingsActivity.java
@@ -1,15 +1,44 @@
 package ath.password_minimizer.activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import ath.password_minimizer.R;
 
 public class PasswordDetailSettingsActivity extends AppCompatActivity {
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_pw_detail_settings:
+                    return true;
+                case R.id.navigation_pw_details_websites:
+                    Intent intentPasswordWebsites = new Intent(PasswordDetailSettingsActivity.this, PasswordDetailWebsitesActivity.class);
+                    startActivity(intentPasswordWebsites);
+                    finish();
+                    return true;
+            }
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_detail_settings);
+        setNavigationBar();
+    }
+
+    private void setNavigationBar() {
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setSelectedItemId(R.id.navigation_pw_detail_settings);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
--- app/src/main/java/ath/password_minimizer/activities/PasswordDetailWebsitesActivity.java
+++ app/src/main/java/ath/password_minimizer/activities/PasswordDetailWebsitesActivity.java
@@ -1,15 +1,46 @@
 package ath.password_minimizer.activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import ath.password_minimizer.R;
 
 public class PasswordDetailWebsitesActivity extends AppCompatActivity {
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_pw_details_websites:
+                    return true;
+                case R.id.navigation_pw_detail_settings:
+                    Intent intentPasswordSettings = new Intent(PasswordDetailWebsitesActivity.this, PasswordDetailSettingsActivity.class);
+                    startActivity(intentPasswordSettings);
+                    finish();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_detail_websites);
+
+        setNavigationBar();
+    }
+
+    private void setNavigationBar() {
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setSelectedItemId(R.id.navigation_pw_details_websites);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
