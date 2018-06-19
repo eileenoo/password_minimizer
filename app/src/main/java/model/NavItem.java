bbfcc31bb0c63591b0ced370866b4053c165aa0f
Jennifer <j.busta@online.de>
2018-06-19 11:00:30 +0200

    Added burger menu to start activity

--- app/src/main/java/model/NavItem.java
+++ app/src/main/java/model/NavItem.java
@@ -1,4 +1,13 @@
 package model;
 
 public class NavItem {
+    public int mTitle;
+    public int mSubtitle;
+    public int mIcon;
+
+    public NavItem(int title, int subtitle, int icon) {
+        mTitle = title;
+        mSubtitle = subtitle;
+        mIcon = icon;
+    }
 }
