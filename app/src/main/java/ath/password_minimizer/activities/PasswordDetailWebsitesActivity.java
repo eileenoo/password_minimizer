f9c1bf01bf123bb61332c27cdf97607c63ad525e
Jennifer <j.busta@online.de>
2018-07-02 21:22:50 +0200

    Added list of websites

--- app/src/main/java/ath/password_minimizer/activities/PasswordDetailWebsitesActivity.java
+++ app/src/main/java/ath/password_minimizer/activities/PasswordDetailWebsitesActivity.java
@@ -6,10 +6,25 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import ath.password_minimizer.R;
+import listAdapters.PasswordListAdapter;
+import listAdapters.WebsiteListAdapter;
+import model.PicturePassword;
+import model.WebsiteCredentials;
 
 public class PasswordDetailWebsitesActivity extends BaseActivity {
+
+    ArrayList<WebsiteCredentials> websites;
+    ListView websitesListView;
+    private static WebsiteListAdapter websiteListAdapter;
+    ImageButton addWebsiteButton;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -36,8 +51,33 @@ public class PasswordDetailWebsitesActivity extends BaseActivity {
         setContentView(R.layout.activity_password_detail_websites);
 
         initBurgerMenu();
-
         setNavigationBar();
+
+        websitesListView = (ListView) findViewById(R.id.website_list);
+
+        websites = new ArrayList<>();
+        websites.add(new WebsiteCredentials("Facebook", "Sam Smith", "password123!"));
+        websites.add(new WebsiteCredentials("Twitter", "Sam Smith", "password123!"));
+        websites.add(new WebsiteCredentials("Amazon", "Sam Smith", "password123!"));
+        websites.add(new WebsiteCredentials("Deutsche Bahn", "Sam Smith", "password123!"));
+
+        websiteListAdapter = new WebsiteListAdapter (websites, getApplicationContext());
+        websitesListView.setAdapter(websiteListAdapter);
+
+        websitesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // TODO: go to edit password
+            }
+        });
+
+        addWebsiteButton = (ImageButton) findViewById(R.id.addWebsiteButton);
+        addWebsiteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: go to create password
+            }
+        });
     }
 
     private void setNavigationBar() {
--- app/src/main/java/model/PicturePassword.java
+++ app/src/main/java/model/PicturePassword.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+
 /**
  * Created by VerenaSchlott on 05/06/18.
  */
@@ -10,12 +12,13 @@ public class PicturePassword {
     public String passwordName;
     public PasswordStrength passwordStrength;
     public String imagePath;
-
+    public ArrayList<WebsiteCredentials> websites;
 
     public PicturePassword(String passwordName, PasswordStrength passwordStrength, String imagePath) {
         this.passwordName = passwordName;
         this.passwordStrength = passwordStrength;
         this.imagePath = imagePath;
+        this.websites = new ArrayList<>();
     }
 
     public String getPasswordName() {
@@ -40,5 +43,28 @@ public class PicturePassword {
 
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
+    }
+
+    public void addWebsite(WebsiteCredentials websiteCredentials) {
+        if (getWebsiteByName(websiteCredentials.getName()) == -1) {
+            websites.add(websiteCredentials);
+        }
+    }
+
+    public void removeWebsite(WebsiteCredentials websiteCredentials) {
+        int websiteIndex = getWebsiteByName(websiteCredentials.getName());
+        if (websiteIndex != -1) {
+            websites.remove(websiteIndex);
+        }
+    }
+
+    public int getWebsiteByName(String name) {
+        for (int i = 0; i < websites.size(); i++) {
+            if (websites.get(i).getName() == name) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
